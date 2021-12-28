7850e09a7a61f4087e618aba34c2192b4ee670e1
Pedro Marchetti Casteli <pedro.casteli@zaffarinet.com.br>
2021-12-28 10:04:44 -0300

    Lecture 52

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./Components/Expenses/Expenses";
+import NewExpense from "./Components/NewExpense/NewExpense";
 
 function App() {
     const expenses = [
@@ -30,6 +31,7 @@ function App() {
 
     return (
         <div>
+            <NewExpense />
             <Expenses item={expenses} />
         </div>
     );
--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    const expenseDate = new Date(2021, 4, 28);
-    const expenseTitle = "Luke Skywalker";
+    const [title, setTitle] = useState(props.title_prop);
+
+    const clickHander = () => {
+        setTitle("Updated!");
+        alert(title);
+    };
 
     return (
         <Card classe="expense-item">
             <ExpenseDate date_prop_2={props.date_prop} />
             <div className="expense-item__description">
-                <h2>{props.title_prop}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">
                     R$ {props.amount_prop}
                 </div>
             </div>
+            <button onClick={clickHander}>Change title</button>
         </Card>
     );
 };
